68df7257993ea03db08f50b9ea8ee345b0ba43d9
Zach Brown <54756689+zbrown36@users.noreply.github.com>
2020-04-14 22:19:46 -0400

    mapped through data for post

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,15 +1,18 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+import React,{ useState } from "react";
 import Post from "./Post";
 import "./Posts.css";
 // import data 
 
-const PostsPage = () => {
+const PostsPage = ({data}) => {
   // set up state for your data
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
+    {data.map((post) => {
+    return <Post post = {post} />
+  })}
     </div>
   );
 };
